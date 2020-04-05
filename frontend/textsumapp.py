1944f540295c74e061d24d500dbac029f9bd00c8
friedTree <jnqtri@gmail.com>
2020-04-05 22:01:26 +1000

    update port

--- frontend/textsumapp.py
+++ frontend/textsumapp.py
@@ -3,5 +3,5 @@ from waitress import serve
 
 if __name__ == "__main__":
     app.debug = False
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=80)
    # app.run(host='0.0.0.0', port=80)
