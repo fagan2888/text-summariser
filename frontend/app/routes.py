c459f45d99247e77bbd3fd71f884577fcc4fcb6c
friedTree <jnqtri@gmail.com>
2020-04-06 09:27:48 +1000

    implement LSA on server

--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -4,6 +4,7 @@ from app.forms import TextSumForm
 import requests
 import json
 from config import API_ENDPOINT
+from app import extraction
 
 url = API_ENDPOINT.URL
 @app.route('/', methods=['GET', 'POST'])
@@ -17,8 +18,9 @@ def index():
            #"This aosad asdhs das asd a. rthrwf rthwf aefg rocoker ys. adthe ys ys ys habh. bothwe thase athwear ehr this is the tird sentence and this is the last sentence and this is the final sentence and this is the one sentence and this is the waiting sent and this"
             full_text
         } 
-        r = requests.post(url = url, data = json.dumps(data))
-        summarised_text = json.loads(r.text)['summary']
+     #   r = requests.post(url = url, data = json.dumps(data))
+     #   summarised_text = json.loads(r.text)['summary']
+        summarised_text = extraction.app_post(full_text)
         summarised_text = summarised_text.split('\n')
         print(summarised_text)
         return render_template('index.html', title='Home', form=form, summary = summarised_text)
